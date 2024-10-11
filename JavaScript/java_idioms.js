75beba9d2749875f81571ee22f43ed9a421ba586
ayaniwickramaratne <awic0008@student.monash.edu>
2024-10-11 14:11:35 +1100

    updated the html

--- JavaScript/java_idioms.js
+++ JavaScript/java_idioms.js
@@ -17,3 +17,8 @@ var vg_4 = "idioms/drug_misuse.vg.json";
 vegaEmbed('#drug_heatmap', vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_5 = "idioms/behaviours_tree.vg.json";
+vegaEmbed('#drug_heatmap', vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
