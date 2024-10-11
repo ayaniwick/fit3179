78d44311c1730f9ec58bb9fc2a04e6836c3c06ed
ayaniwickramaratne <awic0008@student.monash.edu>
2024-10-11 13:37:40 +1100

    added a new idiom drug misuse

--- JavaScript/java_idioms.js
+++ JavaScript/java_idioms.js
@@ -7,3 +7,13 @@ var vg_2 = "idioms/radial_chart.vg.json";
 vegaEmbed('#radial_chart', vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
+var vg_3 = "idioms/alcohol_heatmap.vg.json";
+vegaEmbed('#alcohol_heatmap', vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "idioms/drug_misuse.vg.json";
+vegaEmbed('#drug_heatmap', vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
